efb70388f81f58a4a892654f67ca3b6aa0214a67
Bobby-Boy <marjabob910@gmail.com>
2020-11-30 15:05:03 +0100

    Enemies
    Enemy#1 base done
    Enemy#2 base done
    Enemy#3 begin

---
+++ CupHead/Assets/Enemies/Scripts/BothersomeBlueberry.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BothersomeBlueberry : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rayLength;
+    [SerializeField] private bool _moveRight = true;
+    [SerializeField] private Transform _floorChecker;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * _speed * Time.deltaTime);
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        RaycastHit2D ContactCheck = Physics2D.Raycast(_floorChecker.position, Vector2.right, _rayLength);
+
+        if(ContactCheck == true)
+        {
+
+            //moves to the left while facing left;
+            if(_moveRight == true)
+            {
+                transform.eulerAngles = new Vector2(0, -180);
+                _moveRight = false;
+            }
+
+
+            //moves to the right while facing right;
+            else
+            {
+                transform.eulerAngles = new Vector2(0, 0);
+                _moveRight = true;
+            }
+        }
+
+    }
+}
+
