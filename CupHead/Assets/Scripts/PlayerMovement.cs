b4ded9cb988e49708544cc35685fa736e0b7fce4
DaneyLB <daney@bb-admin.nl>
2020-12-14 11:46:59 +0100

    Enviorment #1
    Parralax background van 3 layers , script met test player (test player kan uiteraard weg gegooid worden) , script is aanpasbaar vaanuit unity zelf dus hoef je niet meer in te editen.

---
+++ CupHead/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+}
